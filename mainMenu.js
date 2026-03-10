e555d9d1fdebeb0accd2867f81d857ac0afcabfc
BHG-sudo <balazshgeza@gmail.com>
2026-03-10 09:37:53 +0100

    Add files via upload

--- mainMenu.js
+++ mainMenu.js
@@ -1,5 +1,30 @@
-var difficulty = 0;
+let battStats = document.getElementById("battStats");
+// Player class
+class Player{
+  constructor(maxHP, ATK, DF){
+    this.maxHP = maxHP;
+    this.HP = maxHP;
+    this.ATK = ATK;
+    this.DF = DF;
+  }
+  function renewStats(){
+    battStats.children[0].innerText = "HP: "+this.maxHP+"/"+this.HP;
+    battStats.children[1].innerText = "ATK: "+this.ATK
+    battStats.children[2].innerText = "DF: "+this.DF
+  }
+}
+// Enemy class
+class Enemy{
+  constructor(HP, ATK, DF){
+    this.HP = HP;
+    this.ATK = ATK;
+    this.DF = DF; 
+  }
+}
 
+let difficulty = 0;
+let action  = 1;
+const player = new Player(0, 0, 0);
 function menuHandler(event) {
   target = event.target;
   switch (target.innerText) {
@@ -23,25 +48,35 @@ function menuHandler(event) {
       toggleDisabled("deathScreen");
       window.setTimeout(byebye, 5000);
       break;
-    case "Könnyű":
+    case "Könnyű":{
       toggleDisabled("difMenu");
       toggleDisabled("transition");
+      action = 2;
+      player.maxHP, player.HP = 20;
+      player.ATK = 4;
+      player.DF = 3;
       break;
-    case "Közepes":
+    }
+    case "Közepes":{
       difficulty = 1;
       toggleDisabled("difMenu");
       toggleDisabled("transition");
+      action = 2;
       break;
-    case "Nehéz":
+    }
+    case "Nehéz":{
       difficulty = 2;
       toggleDisabled("difMenu");
       toggleDisabled("transition");
       break;
-    case "Lehetetlen":
+    }
+    case "Lehetetlen":{
       difficulty = 3;
       toggleDisabled("difMenu");
       toggleDisabled("transition");
       break;
+    }
+
   }
 }
 function toggleDisabled(x) {
@@ -50,39 +85,54 @@ function toggleDisabled(x) {
 function byebye() {
   window.location.href = ".";
 }
-var menu = document.getElementsByClassName("menuList");
-for (var j = 0; j < menu.length; j++) {
-  for (var i = 0; i < menu[j].children.length; i++) {
+let menu = document.getElementsByClassName("menuList");
+for (let j = 0; j < menu.length; j++) {
+  for (let i = 0; i < menu[j].children.length; i++) {
     menu[j].children[i].addEventListener("click", menuHandler);
-    console.log(i);
   }
 }
+
+
 
 //Battle Handling
 
+console.log(action);
 function battHandler(event) {
   target = event.target;
-
-  switch (target.innerText) {
-    case "Támadás":
-      break;
-    case "Varázslatok":
-      break;
-    case "Védekezés":
-      break;
-    case "Leltár":
-      break;
+  if(action > 0){
+    switch (target.innerText) {
+      case "Támadás":
+        action--;
+        classHandler(player, 1, 10);
+        break;
+      case "Varázslatok":
+        action--;
+        break;
+      case "Védekezés":
+        action--;
+        break;
+      case "Leltár":
+        break;
+    }
+    
   }
 
-  /* check enemy
-    - könnyű diffen: nem használ actiont
-    -nehéz diffen: használ actiont
-  */
 }
-var battStats = document.getElementById("battStats");
-var battUI = document.querySelectorAll("td");
-console.log(battStats.children);
-for (var k = 0; k < battUI.length; k++) {
+
+function classHandler(name, what, change){
+  switch(what){
+    case 1:{
+      console.log(change, name.HP)
+      player.HP = player.HP - change;
+      console.log(name.HP)
+      battStats.children[0].innerText = "HP: "+name.HP;
+    }
+  }
+}
+console.log(battStats.children[0])
+
+let battUI = document.querySelectorAll("td");
+for (let k = 0; k < battUI.length; k++) {
   battUI[k].addEventListener("click", battHandler);
 }
 

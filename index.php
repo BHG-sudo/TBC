ac0cf60417645d53f08dae616cd456398522ff8a
Tükör <balazshgeza@gmail.com>
2026-03-30 11:29:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,156 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "tbc";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM items";
+$result = mysqli_query($conn, $sql);
+$items = [];
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $items[] = $row;
+    }
+}
+
+$sql = "SELECT * FROM spells";
+$result = mysqli_query($conn, $sql);
+$spells = [];
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $spells[] = $row;
+    }
+}
+$alapmappa = getcwd();
+chdir("Assets/Enemy/");
+$enemy = [];
+$mappa = opendir(".");
+while (($fajl = readdir($mappa)) != false) {
+    if ($fajl != "." and $fajl != "..") {
+        $enemy[] = "Assets/Enemy/" . $fajl;
+    }
+}
+closedir($mappa);
+chdir($alapmappa);
+
+chdir("Assets/Transition/");
+$transition = [];
+$mappa = opendir(".");
+while (($fajl = readdir($mappa)) != false) {
+    if ($fajl != "." and $fajl != "..") {
+        $transition[] = "Assets/Transition/" . $fajl;
+    }
+}
+closedir($mappa);
+chdir($alapmappa);
+
+chdir("Assets/Room/");
+$room = [];
+$mappa = opendir(".");
+while (($fajl = readdir($mappa)) != false) {
+    if ($fajl != "." and $fajl != "..") {
+        $room[] = "Assets/Room/" . $fajl;
+    }
+}
+closedir($mappa);
+chdir($alapmappa);
+
+chdir("Assets/Boss/");
+$boss = [];
+$mappa = opendir(".");
+while (($fajl = readdir($mappa)) != false) {
+    if ($fajl != "." and $fajl != "..") {
+        $boss[] = "Assets/Boss/" . $fajl;
+    }
+}
+closedir($mappa);
+chdir($alapmappa);
+?>
+
+
+<script>
+    const phpItemData = <?php echo json_encode($items); ?>;
+    const phpSpellsData = <?php echo json_encode($spells); ?>;
+    const phpEnemyIMG = <?php echo json_encode($enemy); ?>;
+    const phpTransIMG = <?php echo json_encode($transition); ?>;
+    const phpRoomIMG = <?php echo json_encode($room); ?>;
+    const phpBossIMG = <?php echo json_encode($boss); ?>;
+</script>
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>TBC</title>
+</head>
+
+<body>
+    <header>
+        <h1>TBC</h1>
+        <h3>Turn-Based-Combat</h3>
+    </header>
+    <main>
+        <div id="menu" class="menu ">
+            <div id="mainM" class="mainM">
+                <ul>
+                    <li value="1">Játék</li>
+                    <li value="2">Kreditek</li>
+                    <li value="3">Kilépés</li>
+                </ul>
+            </div>
+            <div id="kred" class="kred disabled">
+                <h3>Weboldalt készítette: BHG</h3>
+                <h3>Rajzokat készítette: BF</h3>
+                <p id="kredVissza">Vissza</p>
+            </div>
+            <div id="diffM" class="diffM disabled">
+                <ul>
+                    <li value="4">Könnyű</li>
+                    <li value="5">Közepes</li>
+                    <li value="6">Nehéz</li>
+                    <li value="7">Lehetetlen</li>
+                    <li style="margin-top: 30px;" value="8">Vissza</li>
+                </ul>
+            </div>
+        </div>
+        <div id="story" class="story disabled">
+            <img src="Assets/story.png" alt="Story">
+            <ul>
+                <li id="folyt">Folytatás</li>
+                <li id="megsem">Mégsem</li>
+            </ul>
+        </div>
+        <div id="transition" class="transition disabled"></div>
+        <div id="deathscreen" class="deathscreen disabled"></div>
+        <div id="battle" class="battle disabled">
+            <div id="EnemyContainer" class="EnemyContainer">
+                <p id="EnemyHP" class="EnemyHP"></p>
+                <img alt="Enemy" id="enemy" class="enemy">
+            </div>
+            <img src="Assets/karakter.png" alt="Player" id="player" class="player">
+            <div id="battleUI" class="battleUI">
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <p id="attack">Támadás</p>
+                <p id="spell">Varázslat</p>
+                <p id="defend">Védekezés</p>
+                <p id="inventory">Leltár</p>
+            </div>
+        </div>
+    </main>
+    <script src="JS/core.js"></script>
+    <script src="JS/battle.js"></script>
+</body>
+
+</html>
---
+++ kodok/JS/battle.js
@@ -0,0 +1,220 @@
+class entity {
+  constructor(maxHP, HP, ATK, DF, Items, Spells) {
+    this.maxHP = maxHP;
+    this.HP = HP;
+    this.ATK = ATK;
+    this.DF = DF;
+    this.Items = Items;
+    this.Spells = Spells;
+  }
+  setStats(maxHP, ATK, DF) {
+    this.maxHP = maxHP;
+    this.HP = maxHP;
+    this.ATK = ATK;
+    this.DF = DF;
+  }
+  setItems(Items) {
+    this.Items = Items;
+  }
+  setSpells(Spells) {
+    this.Spells = Spells;
+  }
+  renewUIStats() {
+    playerUIStats[0].innerHTML = "HP " + this.maxHP + "/" + this.HP;
+    playerUIStats[1].innerHTML = "ATK " + this.ATK;
+    playerUIStats[2].innerHTML = "DF " + this.DF;
+  }
+}
+const Player = new entity(0, 0, 0, 0);
+const Enemy = new entity(0, 0, 0, 0);
+
+console.log(phpItemData[0]["item_bonus"]);
+function transition1() {
+  transition.classList.toggle("transition11");
+  transition.classList.toggle("transition12");
+}
+function clInt1() {
+  clearInterval(tranInterval1);
+  transition.classList.toggle("disabled");
+  battle.classList.toggle("disabled");
+  if (transition.classList.contains("transition11")) {
+    transition.classList.toggle("transition11");
+  }
+  if (transition.classList.contains("transition12")) {
+    transition.classList.toggle("transition12");
+  }
+}
+function transition2() {
+  transition.classList.toggle("transition21");
+  transition.classList.toggle("transition22");
+}
+function clInt2() {
+  clearInterval(tranInterval2);
+  transition.classList.toggle("disabled");
+  if (transition.classList.contains("transition21")) {
+    transition.classList.toggle("transition21");
+  }
+  if (transition.classList.contains("transition22")) {
+    transition.classList.toggle("transition22");
+  }
+}
+function enemyHPRenew() {
+  document.getElementById("EnemyHP").innerHTML = Enemy.maxHP + "/" + Enemy.HP;
+}
+function transitionBattle(x) {
+  transition.classList.toggle("disabled");
+  switch (x) {
+    case 1:
+      transition.classList.toggle("transition11");
+      tranInterval1 = setInterval(transition1, 500);
+      setTimeout(clInt1, 2000);
+      break;
+    case 2:
+      transition.classList.toggle("transition21");
+      tranInterval2 = setInterval(transition2, 500);
+      setTimeout(clInt2, 2000);
+      break;
+  }
+}
+
+let enemyIMG = document.getElementById("enemy");
+function makeEnemy(number) {
+  switch (number) {
+    case 0:
+      enemyIMG.src = phpEnemyIMG[0];
+      break;
+    case 1:
+      enemyIMG.src = phpEnemyIMG[1];
+      break;
+    case 2:
+      enemyIMG.src = phpEnemyIMG[2];
+      break;
+    case 3:
+      enemyIMG.src = phpEnemyIMG[3];
+      break;
+    case 4:
+      enemyIMG.src = phpEnemyIMG[4];
+      break;
+    case 5:
+      enemyIMG.src = phpEnemyIMG[5];
+      break;
+    case 6:
+      enemyIMG.src = phpEnemyIMG[6];
+      break;
+    case 7:
+      enemyIMG.src = phpEnemyIMG[0];
+      break;
+    case 8:
+      enemyIMG.src = phpEnemyIMG[1];
+      break;
+    case 9:
+      enemyIMG.src = phpEnemyIMG[2];
+      break;
+  }
+}
+
+let defPlayer = false;
+let defEnemy = false;
+
+function enemyAction() {
+  console.log("enemy köre");
+  let randNum = Math.floor(Math.random() * 10);
+  switch (randNum) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+      console.log("Támad");
+      if (defPlayer == true) {
+        if (Enemy.ATK > Player.DF) {
+          Player.HP = Player.HP - (Enemy.ATK - Player.DF);
+          Player.renewUIStats();
+          defPlayer = false;
+        } else {
+          defPlayer = false;
+        }
+      } else {
+        Player.HP -= Enemy.ATK;
+        Player.renewUIStats();
+      }
+      if (Player.HP <= 0) {
+        battle.classList.toggle("disabled");
+        death();
+      }
+      break;
+    case 5:
+    case 6:
+      console.log("Gyógyít");
+      Enemy.HP = Enemy.HP + parseInt(phpSpellsData[3]["spell_stat"]);
+      if (Enemy.HP > Enemy.maxHP) {
+        Enemy.HP = Enemy.maxHP;
+        enemyHPRenew();
+      }
+      break;
+    case 7:
+    case 8:
+    case 9:
+      console.log("Védekez");
+      defEnemy = true;
+      break;
+    default:
+      console.log("Mi történik");
+      break;
+  }
+  console.log("Player HP: " + Player.HP + "\nEnemy HP: " + Enemy.HP);
+}
+
+let defend = document.getElementById("defend");
+defend.addEventListener("click", defendPlayer);
+function defendPlayer() {
+  defPlayer = true;
+  enemyAction();
+}
+/*
+// kevesebb sebzést kapsz plusz matathatsz a leltárodban
+
+// KÉSÖBB!!!
+// Ha védekezel meg kell csináljál egy minigame-et 
+*/
+let inventory = document.getElementById("inventory");
+inventory.addEventListener("click", inventoryM);
+
+function inventoryM() {
+  alert("Ez a funkció még nem működik!");
+}
+
+/*
+// alabból vannak tárgyak a leltárodba és azokat használhatod
+
+*/
+let attack = document.getElementById("attack");
+attack.addEventListener("click", attackPlayer);
+function attackPlayer() {
+  if (Enemy.HP <= 0) {
+    battle.classList.toggle("disabled");
+    transitionBattle(2);
+  } else {
+    if (defEnemy == true) {
+      if (Player.ATK > Enemy.DF) {
+        Enemy.HP = Enemy.HP - (Player.ATK - Enemy.DF);
+        enemyHPRenew();
+      }
+    } else {
+      Enemy.HP -= Player.ATK;
+      enemyHPRenew();
+    }
+    if (Enemy.HP <= 0) {
+      battle.classList.toggle("disabled");
+      transitionBattle(2);
+    } else {
+      enemyAction();
+    }
+  }
+}
+/*
+let spell = document.getElementById("spell");
+spell.addEventListener("click", spellPlayer);
+
+// Kell egy alap spell és azt lehessen kiválasztani (deltarune)
+*/
